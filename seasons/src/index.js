5c2bb695e1c3e8d50c5bb38a97b81d0e672df1b3
Yifei Zhang <yifei7.zhang@gmail.com>
2020-01-10 15:56:29 -0500

    refactor code to use classes instead of functions

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => console.log(position),
-    err => console.error(err)
-  );
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position),
+      err => console.error(err)
+    );
 
-  return <div>Latitude </div>;
-};
+    return <div> Latitude: </div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
