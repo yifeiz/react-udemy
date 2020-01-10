844e8bfb429b3615ab0d208aef8d2ffbd570cd9b
Yifei Zhang <yifei7.zhang@gmail.com>
2020-01-10 16:39:54 -0500

    update conditional rendering

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,13 +2,33 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  render() {
-    window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
-      err => console.error(err)
-    );
+  constructor(props) {
+    super(props);
 
-    return <div> Latitude: </div>;
+    // THIS IS THE ONLY TIME we do direct assignment to this.state (ALWAYS CALL SETSTATE)
+    this.state = { lat: null, errorMessage: "" };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ lat: position.coords.latitude });
+
+        // WE DID NOT DO THE FOLLOWING:
+        // this.state.lat = position.coords.latitude
+      },
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div> Error: {this.state.errorMessage} </div>;
+    } else if (!this.state.errorMessage && this.state.lat) {
+      return <div> Latitude: {this.state.lat} </div>;
+    } else {
+      return <div> Loading! </div>;
+    }
   }
 }
 
