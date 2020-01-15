57e96ed2db58ba293d842aede39cd4cf44651447
Yifei Zhang <yifei7.zhang@gmail.com>
2020-01-14 21:53:43 -0500

    add image card for theming and refactored umspash auth

--- pics/src/api/unsplash.js
+++ pics/src/api/unsplash.js
@@ -1,10 +1,11 @@
 import axios from "axios";
+import password from "../password";
 
+console.log(password());
 export default axios.create({
   baseURL: "https://api.unsplash.com/",
   headers: {
-    Authorization:
-      // INSERT CLIENT KEY BELOW
-      "Client-ID "
+    // make sure password.js file exists, and has the unsplash key value returned
+    Authorization: password()
   }
 });
