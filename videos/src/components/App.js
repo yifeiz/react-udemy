28f261842bfe57c227e14fcdb7a938deacd223d2
Yifei Zhang <yifei7.zhang@gmail.com>
2020-01-20 12:47:57 -0500

    display videoList

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -2,9 +2,10 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import VideoList from "./VideoList";
 import youtube from "../apis/youtube";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
   onTermSubmit = async term => {
     console.log(term);
     const response = await youtube.get("/search", {
@@ -14,11 +15,21 @@ class App extends React.Component {
     });
     this.setState({ videos: response.data.items });
   };
+
+  onVideoSelect = video => {
+    console.log("From the App!", video);
+    this.setState({ selectedVideo: video });
+  };
+
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={this.onVideoSelect}
+        />
       </div>
     );
   }
--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  state = { term: "jklj;k" };
+  state = { term: "cooperators" };
 
   onInputChange = event => {
     this.setState({
