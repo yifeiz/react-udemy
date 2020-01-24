166af0929c88fe0d8d091d05997278a26e98e5b9
Yifei Zhang <yifei7.zhang@gmail.com>
2020-01-23 22:08:23 -0500

    exploring routers

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Header from "./Header";
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamShow from "./streams/StreamShow";
+import StreamDelete from "./streams/StreamDelete";
+import StreamList from "./streams/StreamList";
 
 const App = () => {
-    return <div> App </div>;
+  return (
+    <div className="ui container">
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/new" component={StreamCreate} />
+          <Route path="/edit" component={StreamEdit} />
+          <Route path="/delete" component={StreamDelete} />
+          <Route path="/show" component={StreamShow} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
