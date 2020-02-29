4673da32d38fc9b5c3ee9c74b979f98138cbf489
Yifei Zhang <yifei7.zhang@gmail.com>
2020-02-28 21:11:17 -0500

    bit more progress on auth

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -1,14 +1,30 @@
 import React from "react";
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
-      window.gapi.client.init({
-        clientId:
-          "30685105305-hfhhfhe0nabu7as9vuu69lq67pod9cn6.apps.googleusercontent.com",
-        scope: "email"
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            "30685105305-hfhhfhe0nabu7as9vuu69lq67pod9cn6.apps.googleusercontent.com",
+          scope: "email"
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
     });
+  }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>Are we signed in?</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>Signed in! </div>;
+    } else {
+      return <div>Not Signed in :( </div>;
+    }
   }
 
   render() {
