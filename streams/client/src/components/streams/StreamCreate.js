6c74ca5a38e963b66048c321db0a7c0ef46c2615
Yifei Zhang <yifei7.zhang@gmail.com>
2020-03-02 21:30:06 -0500

    display redux-form

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -2,9 +2,26 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
+  renderInput({ input, label }) {
+    return (
+      <div className="field">
+        <label> {label} </label>
+        <input {...input} />
+      </div>
+    );
+  }
   render() {
     console.log(this.props);
-    return <div>StreamCreate</div>;
+    return (
+      <form className="ui form">
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
+      </form>
+    );
   }
 }
 
