a374c63e72485ca7db2e6ede77c8fddd53549727
Yifei Zhang <yifei7.zhang@gmail.com>
2020-03-11 23:43:13 -0400

    reusable hooks

--- hooks-simple/src/components/App.js
+++ hooks-simple/src/components/App.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
+
+import UserList from "./UserList";
 import ResourceList from "./ResourceList";
 
 const App = () => {
   const [resource, setResource] = useState("posts");
   return (
     <div>
+      <UserList />
       <div>
         <button onClick={() => setResource("posts")}>Posts</button>
         <button onClick={() => setResource("todos")}>Todos</button>
