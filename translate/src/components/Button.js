9c54f423140080c8a2301fdf0bd96c2a5bf9741b
Yifei Zhang <yifei7.zhang@gmail.com>
2020-03-11 20:35:16 -0400

    consumers and context

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -1,12 +1,32 @@
 import React from "react";
+
 import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 class Button extends React.Component {
-  static contextType = LanguageContext;
+  // Only needed if we use this.context
+  //   static contextType = LanguageContext;
+
+  renderSubmit(value) {
+    return value === "english" ? "Submit" : "提交";
+  }
+
+  renderButton(color) {
+    return (
+      <button className={`ui button ${color}`}>
+        <LanguageContext.Consumer>
+          {value => this.renderSubmit(value)}
+        </LanguageContext.Consumer>
+      </button>
+    );
+  }
 
   render() {
-    const text = this.context === "english" ? "Submit" : "提交";
-    return <button className="ui button primary">{text}</button>;
+    return (
+      <ColorContext.Consumer>
+        {color => this.renderButton(color)}
+      </ColorContext.Consumer>
+    );
   }
 }
 
