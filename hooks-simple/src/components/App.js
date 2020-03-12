02ec749dad6487b3b8050dc835c5f232bdb7a703
Yifei Zhang <yifei7.zhang@gmail.com>
2020-03-11 22:40:53 -0400

    useState and useEffect hooks

--- hooks-simple/src/components/App.js
+++ hooks-simple/src/components/App.js
@@ -1,30 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import ResourceList from "./ResourceList";
 
-class App extends React.Component {
-  state = { resource: "posts" };
-  render() {
-    return (
+const App = () => {
+  const [resource, setResource] = useState("posts");
+  return (
+    <div>
       <div>
-        <div>
-          <button
-            onClick={() => {
-              this.setState({ resource: "posts" });
-            }}
-          >
-            Posts
-          </button>
-          <button
-            onClick={() => {
-              this.setState({ resource: "todos" });
-            }}
-          >
-            Todos
-          </button>
-        </div>
-        {this.state.resource}
+        <button onClick={() => setResource("posts")}>Posts</button>
+        <button onClick={() => setResource("todos")}>Todos</button>
       </div>
-    );
-  }
-}
+      <ResourceList resource={resource} />
+    </div>
+  );
+};
 
 export default App;
